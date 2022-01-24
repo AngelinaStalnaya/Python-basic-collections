5f6981fbcc7748224b0d800320157f0b32419fc1
Angelina Stalnaya <angelinastalnaya.feb@gmail.com>
2022-01-25 00:07:38 +0300

    1-3 completed, 4 - 98%

--- task_01_list_gen/main.py
+++ task_01_list_gen/main.py
@@ -1,7 +1,15 @@
 def main():
-    # TODO здесь писать код
+    number = int(input('Введите число N: '))
+    num_list = []
+
+    for numbers in range(1, number + 1):
+        if numbers % 2 > 0:
+            num_list.append(numbers)
+
+    print('Список из нечётных чисел от 1 до N: ', num_list)
     pass
 
 
 if __name__ == '__main__':
     main()
+
--- task_04_videocards/main.py
+++ task_04_videocards/main.py
@@ -1,5 +1,20 @@
 def main():
-    # TODO здесь писать код
+    cards = int(input('Кол-во видеокарт: '))
+    old_list = []
+    for card in range(1, cards + 1):
+        print(card, 'Видеокарта: ', end='')
+        SNo = int(input())
+        old_list.append(SNo)
+    new_list = old_list
+    all_cards = len(new_list)
+    id = max(new_list)
+    for i in range(all_cards + 1):
+        print(new_list[i])
+        if new_list[i] == id:
+            new_list.pop(i)
+
+    print('Старый список видеокарт: ', old_list)
+    print('Новый список видеокарт: ', new_list)
     pass
 
 
