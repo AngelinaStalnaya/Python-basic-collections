b0ca097c55a957d5207b697422c7e9f41ef3424b
Артем Семенычев <smetatsc@yandex.ru>
2022-02-07 11:07:07 +0000

    fix 3

--- task_03_cells/main.py
+++ task_03_cells/main.py
@@ -1,13 +1,16 @@
 def main():
-    ceils = int(input('Кол-во клеток: '))
-    wrong_data = []
-    for i in range(ceils):
-        print('Эффективность', i + 1, 'клетки:', end= '')
-        kpd = int(input())
-        if kpd < i:
-            wrong_data.append(kpd)
-    print('Неподходящие значения:', wrong_data)
-    pass
+    cells_num = int(input('Кол-во клеток: '))
+    effic_list = []
+    for i_cell in range(cells_num):
+        print('Эффективность', i_cell + 1, 'клетки:', end=' ')
+        efficiency = int(input())
+        effic_list.append(efficiency)
+
+    res = []
+    for i_rank in range(cells_num):
+        if effic_list[i_rank] < i_rank:
+            res.append(str(effic_list[i_rank]))
+    print('Неподходящие значения: ' + " ".join(res))
 
 
 if __name__ == '__main__':
