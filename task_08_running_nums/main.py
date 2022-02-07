9b41a252713ae2403b3d1a89eb278ae20124351d
Артем Семенычев <smetatsc@yandex.ru>
2022-02-07 16:38:03 +0000

    fix 8

--- task_08_running_nums/main.py
+++ task_08_running_nums/main.py
@@ -1,9 +1,17 @@
-def main():
-    step = int(input('Сдвиг: '))
-    my_list = [1, 2, 3, 4, 5]
-    new_list = my_list[-step:] + my_list[:-step]
-    print('Сдвинутый список:', new_list)
+def shift(lst, steps):
+    for _ in range(steps):
+        temp_lst = lst[len(lst) - 1]
+        for i in range(len(lst) - 2, -1, -1):
+            lst[i + 1] = lst[i]
+        lst[0] = temp_lst
+    return lst
 
+
+def main():
+    shift_count = int(input("Сдвиг: "))
+    str_list = input("Изначальный список: ")
+    nums = [int(i) for i in str_list[1:-1].split(', ')]
+    print("Сдвинутый список:", shift(nums, shift_count))
 
 
 if __name__ == '__main__':
